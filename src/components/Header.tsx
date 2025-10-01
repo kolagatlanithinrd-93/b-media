337d9f66711ca60194e9d97ab38ee3719534c2b9
kolagatlanithinrd-93 <kolagatlanithinrd@gmail.com>
2025-10-01 11:00:40 +0530

    Remove Second Search Bar and Fix JSX Error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,18 +48,9 @@ export default function Header({ onSignOut }: HeaderProps) {
             <UserDropdown onSignOut={onSignOut} />
           </div>
         </div>
-
-        <div className="md:hidden mt-3">
-          <div className="relative">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
-            <input
-              type="text"
-              placeholder="Search posts..."
-              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-            />
-          </div>
-        </div>
       </div>
+
+      <MessagesModal isOpen={isMessagesOpen} onClose={() => setIsMessagesOpen(false)} />
     </header>
-  )
+  );
 }
